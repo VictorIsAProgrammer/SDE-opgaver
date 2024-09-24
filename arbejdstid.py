75d480d26e45e3ec1b14b8c739fa1b5a11eae4e9
PC14 <asdf@xd.dx>
2024-09-24 11:12:11 +0200

    verify_hour() & verify_minute() now correctly returns true/false (fixed infinite recursive loop)
    now properly handles hours/minutes (instead of printing negative minutes)

--- arbejdstid.py
+++ arbejdstid.py
@@ -1,51 +1,66 @@
-def verify_time_valid_time(time):
+def input_time(time_unit):
     # check om timer er korrekte
-    if time == "hour":
-        hour = input()
+    if time_unit == "hour":
+        hour = input("Time: ")
         try:
             hour = int(hour)
         except:
             print("Indtast time som tal")
-            return verify_time_valid_time("hour")
-        if not verify_hour(int(hour)):
-            return verify_time_valid_time("hour")
+            return input_time("hour")
+        if not verify_hour(hour):
+            return input_time("hour")
         return hour
     # check om minut er korrekt
-    elif time == "minute": 
-        minute = input()
+    elif time_unit == "minute":
+        minute = input("Minut: ")
         try:
             minute = int(minute)
         except:
             print("Indtast time som tal")
-            return verify_time_valid_time("minute")
+            return input_time("minute")
         if not verify_minute(int(minute)):
-            return verify_time_valid_time("minute")
+            return input_time("minute")
         return minute
 
 def verify_hour(h):
-    if(h >= 0 and h <= 23):
-        return h
+    if 0 <= h <= 23:
+        return True
     else:
         print("Indtast time imellem 0 og 23")
-        return(verify_time_valid_time("hour"))
+        return False
     
 def verify_minute(m):
-    if(m >= 0 and m <= 59):
-        return m
+    if 0 <= m <= 59:
+        return True
     else:
         print("Indtast time imellem 0 og 59")
-        return(verify_time_valid_time("minute"))
+        return False
+
+def time_calc(h1, h2, m1, m2):
+    hours = h2 - h1
+    minutes = m2 - m1
+    if hours < 0:
+        hours += 24
+    if minutes < 0:
+        minutes += 60
+        hours -= 1
+        if hours < 0:
+            hours += 24
+
+    return hours, minutes
 
 
 print("indtast ankomsttime")
-aHour = verify_time_valid_time("hour")
+aHour = input_time("hour")
 print("indtast ankomstminut")
-aMinute = verify_time_valid_time("minute")
+aMinute = input_time("minute")
 print("indtast afgangstime")
-lHour = verify_time_valid_time("hour")
+lHour = input_time("hour")
 print("indtast afgangsminut")
-lMinute = verify_time_valid_time("minute")
-# print("test values: aHour = " + str(aHour) + ", lHour = " + str(lHour)) # test
+lMinute = input_time("minute")
 
-# håndterer ikke hvis ankomst (minut eller time) er højere end afgang.
-print("du har arbejdet " + str(lHour - aHour) + " timer og " + str(lMinute - aMinute) + " minutter")
+time_stayed = time_calc(aHour, lHour, aMinute, lMinute)
+
+print(f"Du har arbejdet {time_stayed[0]} timer og {time_stayed[1]} minutter.")
+
+
